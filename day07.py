425e47ccef5a32897ce4891a19f39cb748b8bc9b
Alex Pettifer <ap14g20@soton.ac.uk>
2021-12-07 11:16:53 +0000

    Made day 7 nicer, with less repeated code

--- day07.py
+++ day07.py
@@ -6,20 +6,7 @@ class Day7(AdventDay):
     def __init__(self):
         super().__init__(2021, 7)
 
-    def part_1(self):
-        crabs = self.parse_ints(True)
-
-        lowest_fuel_count = float('inf')
-        for dist in range(max(crabs)):
-            fuel = 0
-            for crab in crabs:
-                fuel += abs(crab - dist)
-
-            if fuel < lowest_fuel_count:
-                lowest_fuel_count = fuel
-        return lowest_fuel_count
-
-    def part_2(self):
+    def solve(self, part):
         crabs = self.parse_ints(True)
 
         lowest_fuel_count = float('inf')
@@ -27,11 +14,22 @@ class Day7(AdventDay):
             fuel = 0
             for crab in crabs:
                 to_move = abs(crab - dist)
-                fuel += (to_move * (to_move + 1)) // 2
+
+                if part == 1:
+                    fuel += to_move
+                else:
+                    fuel += (to_move * (to_move + 1)) // 2
 
             if fuel < lowest_fuel_count:
                 lowest_fuel_count = fuel
         return lowest_fuel_count
+
+    def part_1(self):
+        return self.solve(part=1)
+
+    def part_2(self):
+        return self.solve(part=2)
+
 
 
 if __name__ == '__main__':
